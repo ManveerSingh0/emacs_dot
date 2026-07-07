203145079680623f7810381478dae31819d23426
ManveerSingh0 <singhcoder2005@gmail.com>
2026-07-07 23:46:33 +0530

    added golden ratio package let's see how does it works

--- init.el
+++ init.el
@@ -4,3 +4,15 @@
 (org-babel-load-file
 	(expand-file-name "config.org" user-emacs-directory))
 (put 'magit-edit-line-commit 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/.zuhitsu/20260502191748-todo.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
